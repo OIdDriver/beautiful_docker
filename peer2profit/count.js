d11e4d4e17a7decb71f8bfed9474a7b7bcef9048
HelloWorld <zyf364@gmail.com>
2021-08-26 16:05:51 +0800

    Update count.js
    加强版-按ip统计

--- peer2profit/count.js
+++ peer2profit/count.js
@@ -1,17 +1,59 @@
-temp1.peers.sort(function (a, b) {
-  if (a.totalTraffic.indexOf('MB')>-1 && b.totalTraffic.indexOf('KB')>-1){
-	return 1
-  }
-  else if (a.totalTraffic.indexOf('MB')>-1 && b.totalTraffic.indexOf('MB')>-1){
-	return parseFloat(a.totalTraffic.replace(' MB',''))-parseFloat(b.totalTraffic.replace(' MB',''))
-  }
-  else if (a.totalTraffic.indexOf('KB')>-1 && b.totalTraffic.indexOf('MB')>-1){
-	return -1
-  }
-  else if (a.totalTraffic.indexOf('KB')>-1 && b.totalTraffic.indexOf('KB')>-1){
-	return parseFloat(a.totalTraffic.replace(' KB',''))-parseFloat(b.totalTraffic.replace(' KB',''))
-  }
+const  groupBy = (array, f) => {
+    let groups = {};
+    array.forEach(function (o) {
+        var group = JSON.stringify(f(o));
+        groups[group] = groups[group] || [];
+        groups[group].push(o);
+    });
+    return Object.keys(groups).map(function (group) {
+        return groups[group];
+    });
+};
+ 
+const arrayGroupBy = (list, groupId) => {
+    let sorted = groupBy(list, function (item) {
+        return [item[groupId]];
+    });
+    return sorted;
+};
+
+const converToM = (traffic) => {
+	let numStr = traffic.replace(' KB','').replace(' MB','');
+	if(traffic.indexOf('GB')>-1){
+		return parseFloat(numStr)*1024;
+	}
+    else if(traffic.indexOf('MB')>-1){
+		return parseFloat(numStr);
+	}
+	else if(traffic.indexOf('KB')>-1){
+		return parseFloat(numStr)/1024;
+	}
+	else{
+		return 0;
+	}
+};
+
+let listData = temp1 || temp1.peers;
+let ipList = arrayGroupBy(listData,'ip'); 
+let newList = [];
+for(i=0;i<ipList.length;i++){
+	if(ipList[i] && ipList[i].length>0){
+		let newHost = JSON.parse(JSON.stringify(ipList[i][0]));		
+		newHost.totalTraffic = converToM(newHost.totalTraffic);
+		newHost.sub=[];
+		newHost.sub.push(ipList[i][0]);
+		for(j=1;j<ipList[i].length;j++){
+			let host = ipList[i][j];
+			newHost.totalTraffic += converToM(host.totalTraffic);
+			newHost.sub.push(host);
+		};
+		newList.push(newHost);
+	}
+};
+newList.sort(function (a, b) {
+  return a.totalTraffic-b.totalTraffic;
 });
-for(i=0;i<temp1.peers.length;i++){
-    console.info(temp1.peers[i].type,temp1.peers[i].ip,temp1.peers[i].isp,temp1.peers[i].totalTraffic);
-}
+
+for(i=0;i<newList.length;i++){
+    console.info(newList[i].type,newList[i].ip,newList[i].isp,Math.floor(newList[i].totalTraffic) + 'M');
+};
